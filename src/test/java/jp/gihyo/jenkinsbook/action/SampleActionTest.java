e22bdca8b53f1eece480bf4ed8765769adb1887a
kanno <kanno@192.168.10.111>
2019-07-28 13:25:41 +0900

    テスト追加

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -75,10 +75,13 @@ public class SampleActionTest {
 
 		assertEquals(false, result);
 	}
+
+	@Test
 	public void testCheckError4() {
 		SampleAction action = new SampleAction();
 		HttpServletRequest request = createMock(HttpServletRequest.class);
 
+		expect(request.getParameter("FirstName")).andReturn("firstName");
 		expect(request.getParameter("LastName")).andReturn("");
 
 		replay(request);
